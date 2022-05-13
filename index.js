2ed229819cbb485b47b5011654d248b9e543f538
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-05-13 06:21:05 +0600

    add product && get product to DB api ready

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+const fileUpload = require("express-fileupload");
 
 const port = process.env.PORT || 4040;
 
@@ -10,9 +11,10 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
-  res.send("Hello ak World!");
+  res.send("Hello World!");
 });
 
 const client = new MongoClient(uri, {
@@ -26,14 +28,35 @@ async function run() {
     await client.connect();
     const db = client.db("homeDecorDB");
     const productCollection = db.collection("products");
-    const doc = {
-      name: "solaman kan",
-      email: "bhoi@gmail.com",
-    };
-    const result = await productCollection.insertOne(doc);
-    console.log(result);
+
+    // get all products from db
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      res.json(products);
+    });
+
+    // add single product to db
+    app.post("/addproduct", async (req, res) => {
+      const name = req.body.name;
+      const price = req.body.price;
+      const catagory = req.body.catagory;
+      const image = req.files.image;
+      const imageData = image.data;
+      const encodedImage = imageData.toString("base64");
+      const imageBuffer = Buffer.from(encodedImage, "base64");
+
+      const product = {
+        name,
+        price,
+        catagory,
+        image: imageBuffer,
+      };
+
+      const result = await productCollection.insertOne(product);
+      res.json(result);
+    });
   } finally {
-    await client.close();
   }
 }
 run().catch(console.dir());
